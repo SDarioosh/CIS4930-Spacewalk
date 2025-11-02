7b48fe5af00cfbd38556283bc2407a068131b344
Shordore <s.darioosh@ufl.edu>
2025-11-01 21:05:31 -0400

    Added space station interior and functioning teleport system

--- Assets/TeleportButton.cs
+++ Assets/TeleportButton.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class TeleportButton
+// You might need this namespace if you are using the XROrigin component
+// using Unity.XR.CoreUtils; 
+
+public class TeleporterButton : MonoBehaviour
 {
-    
+    [Tooltip("The XR Origin (your player) to teleport.")]
+    public GameObject xrOrigin;
+
+    [Tooltip("The exact coordinates to teleport the player to.")]
+    public Vector3 targetCoordinates;
+
+    // This is the public function we will call from the button event
+    public void TeleportPlayer()
+    {
+        if (xrOrigin != null)
+        {
+            // Instantly move the player's origin to the target coordinates
+            xrOrigin.transform.position = targetCoordinates;
+        }
+        else
+        {
+            Debug.LogWarning("XR Origin is not assigned in the TeleporterButton script!");
+        }
+    }
 }
